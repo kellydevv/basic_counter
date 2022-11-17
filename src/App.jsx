d8e0e137f801f95c24590118abb429159bf72d2d
정현진 <jhj9328@hanmail.net>
2022-11-17 18:16:58 +0900

    더하기,뺴기 버튼으로도 할 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [value,setValue] = useState(0);
+  const [value, setValue] = useState(0);
 
   const PlusOne = (event) => {
-const inputValue = event.target.value;
-setValue(value+1);
-  }
+    const inputValue = event.target.value;
+    setValue(value + 1);
+  };
 
   const MinusOne = (evnet) => {
-setValue(value-1);
-  }
+    setValue(value - 1);
+  };
 
   return (
     <div>
       <p>{value}</p>
       <button onClick={PlusOne}>+1</button>
-      <button onClick ={MinusOne}>-1</button>
+      <button onClick={MinusOne}>-1</button>
+      <p/>
+      <button
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        더하기 일
+      </button>
+      <button
+        onClick={() => {
+          setValue(value - 1);
+        }}
+      >
+        빼기 일
+      </button>
     </div>
-  )
-};
+  );
+}
 
 export default App;
